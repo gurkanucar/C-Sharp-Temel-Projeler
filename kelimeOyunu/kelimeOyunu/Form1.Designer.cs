f4119b62b003cc8f9f2c93b5267cfde3edaa302f
Gurkan UCAR <gurkanucar@trakya.edu.tr>
2020-04-26 17:23:11 +0300

    Add files via upload

---
+++ kelimeOyunu/kelimeOyunu/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace kelimeOyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 25F);
+            this.label1.Location = new System.Drawing.Point(33, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 57);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "KELİME";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 30F);
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(204, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 67);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "10";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(168, 287);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(154, 47);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Kontrol Et";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(168, 367);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(154, 42);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Pas";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(92, 206);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(289, 41);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(37, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 35);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "0";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(168, 438);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(154, 47);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Başla";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 35F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(485, 519);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ kelimeOyunu/kelimeOyunu/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kelimeOyunu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int puan = 0;
+        string secilen;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            Karistir();
+
+
+        }
+
+        Random rnd = new Random();
+        public void Karistir()
+        {
+            String[] kelimeler = {"elma","armut","televizyon","bilgisayar","kalem","peçete","zımba","makas","pano" };
+
+            secilen = kelimeler[rnd.Next(0, kelimeler.Length)].ToUpper();
+
+            List<String> harfler = new List<string>();
+
+            foreach (var item in secilen)
+            {
+                harfler.Add(item.ToString());
+            }
+
+
+            for (int i = 0; i < secilen.Length; i++)
+            {
+                int a = rnd.Next(0, secilen.Length);
+                int b = rnd.Next(0, secilen.Length);
+
+                int tekrar = rnd.Next(3, 20);
+
+                for (int j = 0; j < tekrar; j++)
+                {
+                    string gecici = harfler[a];
+                    harfler[a] = harfler[b];
+                    harfler[b] = gecici;
+                    
+                }
+
+            }
+
+            string sonHal = "";
+
+            foreach (var item in harfler)
+            {
+                sonHal += item;
+            }
+
+            if(secilen==sonHal)
+            {
+                Karistir();
+            }
+            else
+            {
+                label1.Text = sonHal;
+            }
+           
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            puan -= 5;
+            label3.Text = puan + "";
+            Karistir();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(secilen==textBox1.Text.ToUpper())
+            {
+                puan += 10;
+              
+            }
+            else
+            {
+                puan-=7;
+            }
+
+              label3.Text = puan+"";
+              Karistir();
+              textBox1.Text = "";
+        }
+        int sayac = 10;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(sayac>0)
+            {
+                sayac--;
+                label2.Text = sayac + "";
+            }
+            else
+            {
+                timer1.Enabled = false;
+                MessageBox.Show("Puanınız:"+puan,"OYUN");
+
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            sayac = 10;
+            timer1.Enabled = true;
+            puan = 0;
+            label3.Text = puan + ""; ;
+            label2.Text = sayac + "";
+            textBox1.Text = "";
+            Karistir();
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (secilen == textBox1.Text.ToUpper())
+            {
+                puan += 10;
+                label3.Text = puan + "";
+                Karistir();
+                textBox1.Text = "";
+
+            }
+         
+           
+        }
+
+
+    }
+}
